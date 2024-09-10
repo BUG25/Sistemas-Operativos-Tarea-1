a79cfbef5a02614eda3c8391e456b344b0e8ed8d
Nodercif <141868003+Nodercif@users.noreply.github.com>
2024-09-09 22:19:43 -0300

    Update favs.h

--- favs.h
+++ favs.h
@@ -4,20 +4,19 @@
 #define MAX_INPUT_LENGTH 1024
 
 typedef struct{
-    int id; //Cada comando debe además tener asociado un número que permita identificarlo
+    int id; // Cada comando debe además tener asociado un número que permita identificarlo
     char command[MAX_INPUT_LENGTH];
-}Fav;
-
+} Fav;
 
 // Funciones para el manejo de favoritos
 void favs_crear();
-void favs_agregar();
+void favs_agregar(char *command);
 void favs_mostrar();
 int esNumerica(const char *cadena);
 void favs_eliminar();
 void favs_buscar(const char *cmd);
 void favs_borrar();
-void favs_ejecutar();
+void favs_ejecutar(int id); // Modificado para recibir un ID
 void favs_cargar();
 void favs_guardar();
 
